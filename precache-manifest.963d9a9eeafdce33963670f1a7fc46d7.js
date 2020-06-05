df39a7f9534942db8e9a4ead53074fbaec4aabe9
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-05 20:02:14 +0200

    Updates

---
+++ precache-manifest.963d9a9eeafdce33963670f1a7fc46d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dfcbf5b72e26205650f72ebaf17ae7",
+    "url": "/robofriends-typescript/index.html"
+  },
+  {
+    "revision": "3e5d8f40fac6237667ba",
+    "url": "/robofriends-typescript/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d377215f269100a87f09",
+    "url": "/robofriends-typescript/static/css/main.6d3863fa.chunk.css"
+  },
+  {
+    "revision": "3e5d8f40fac6237667ba",
+    "url": "/robofriends-typescript/static/js/2.f89077f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-typescript/static/js/2.f89077f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d377215f269100a87f09",
+    "url": "/robofriends-typescript/static/js/main.e2b4724c.chunk.js"
+  },
+  {
+    "revision": "2c0aed8b7562f9575f6c",
+    "url": "/robofriends-typescript/static/js/runtime-main.a95c556a.js"
+  },
+  {
+    "revision": "229902fd3e812267f244130b774eab9e",
+    "url": "/robofriends-typescript/static/media/Super Mario Bros..229902fd.woff"
+  }
+]);
